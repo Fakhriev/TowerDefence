bfda5489f9c10081351a22c1641cca2f33f765fb
Dinar Fakhriev <dinar.fahriev@yandex.ru>
2021-01-07 21:49:18 +0300

    Divide Enemy Movement to Other Class

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [Header("References")]
+    [SerializeField] private EnemyMovement EnemyMovement;
     [SerializeField] private EnemyHealth EnemyHealth;
 
     [Header("Enemy Components")]
@@ -17,64 +18,25 @@ public class Enemy : MonoBehaviour
     [Header("Enemy Parametres")]
     [SerializeField] private float deactivateTimer;
 
-    public delegate void MethodContainer();
-    public event MethodContainer OnDie;
+    private EnemyData myEnemyData;
 
-    private MovePoint[] movePointsArray;
-    private MovePoint nextMovePoint;
-
-    private bool isMoving;
     private bool isDead;
 
-    /// <summary>
-    /// Fields From EnemyData
-    /// </summary>
-    public EnemyData myEnemyData;
-    private Animator animator;
-    private float speed;
-
-    private void Update()
-    {
-        if (isMoving == false)
-            return;
-
-        Vector3 newPosition = Vector3.MoveTowards(transform.position, nextMovePoint.position, speed * Time.deltaTime);
-        transform.position = newPosition;
-
-        if (transform.position == nextMovePoint.position)
-            SetNewNextMovePoint();
-    }
-
-    private void SetNewNextMovePoint()
-    {
-        if(movePointsArray[nextMovePoint.index].type == MovePointType.FinishPoint)
-        {
-            StopEnemy(EnemyAnimationTrigger.Attack);
-            return;
-        }
-
-        transform.LookAt(movePointsArray[nextMovePoint.index + 1].transform);
-        nextMovePoint = movePointsArray[nextMovePoint.index + 1];
-    }
-
-    private void StopEnemy(EnemyAnimationTrigger animTrigger)
-    {
-        isMoving = false;
-        animator.SetTrigger(animTrigger.ToString());
-    }
+    public delegate void MethodContainer();
+    public event MethodContainer OnDie;
 
     private void SetupEnemyMeshPrefab()
     {
         GameObject createdMeshPrefab = Instantiate(myEnemyData.MeshPrefab, meshPrefabParent);
         EnemyMeshPrefab enemyMeshPrefab = createdMeshPrefab.GetComponent<EnemyMeshPrefab>();
-        animator = enemyMeshPrefab.Animator;
+        EnemyMovement.SetupAnimator(enemyMeshPrefab.Animator);
     }
 
     private void SetupEnemyStats()
     {
+        EnemyMovement.SetupSpeed(myEnemyData.EnemyStats.Speed);
         EnemyHealth.SetEnemyhealth(myEnemyData.EnemyStats.Health);
         //EnemyAttack.SetEnemyDamage(myEnemyData.EnemyStats.DamagePerSecond); TODO
-        speed = myEnemyData.EnemyStats.Speed;
     }
 
     private IEnumerator MakeEnemyNonActives()
@@ -94,7 +56,6 @@ public class Enemy : MonoBehaviour
             return;
 
         boxCollider.enabled = false;
-        StopEnemy(EnemyAnimationTrigger.Die);
 
         isDead = true;
         OnDie?.Invoke();
@@ -110,20 +71,8 @@ public class Enemy : MonoBehaviour
         SetupEnemyMeshPrefab();
         SetupEnemyStats();
 
-        movePointsArray = movePoints;
-
-        transform.position = Array.Find(movePointsArray, point => point.type == MovePointType.StartPoint).position;
-        nextMovePoint = Array.Find(movePointsArray, point => point.type == MovePointType.Point);
-
-        transform.LookAt(nextMovePoint.transform);
-        isMoving = true;
+        EnemyMovement.SetupMovePoints(movePoints);
     }
-}
-
-public enum EnemyAnimationTrigger
-{
-    Attack,
-    Die
 }
 
 [Serializable]
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Enemy Enemy;
+
+    private Animator animator;
+    private float speed;
+
+    private MovePoint[] movePointsArray;
+    private MovePoint nextMovePoint;
+
+    private bool isMoving;
+
+    private void Start()
+    {
+        Enemy.OnDie += EnemyDie;
+    }
+
+    private void Update()
+    {
+        if (isMoving == false)
+            return;
+
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, nextMovePoint.position, speed * Time.deltaTime);
+        transform.position = newPosition;
+
+        if (transform.position == nextMovePoint.position)
+            SetNewNextMovePoint();
+    }
+
+    private void SetNewNextMovePoint()
+    {
+        if (movePointsArray[nextMovePoint.index].type == MovePointType.FinishPoint)
+        {
+            StopEnemy(EnemyAnimationTrigger.Attack);
+            return;
+        }
+
+        transform.LookAt(movePointsArray[nextMovePoint.index + 1].transform);
+        nextMovePoint = movePointsArray[nextMovePoint.index + 1];
+    }
+
+    private void StopEnemy(EnemyAnimationTrigger animTrigger)
+    {
+        isMoving = false;
+        animator.SetTrigger(animTrigger.ToString());
+    }
+
+    private void EnemyDie()
+    {
+        Enemy.OnDie -= EnemyDie;
+        StopEnemy(EnemyAnimationTrigger.Die);
+    }
+
+    public void SetupMovePoints(MovePoint[] movePoints)
+    {
+        movePointsArray = movePoints;
+
+        transform.position = Array.Find(movePointsArray, point => point.type == MovePointType.StartPoint).position;
+        nextMovePoint = Array.Find(movePointsArray, point => point.type == MovePointType.Point);
+
+        transform.LookAt(nextMovePoint.transform);
+        isMoving = true;
+    }
+
+    public void SetupAnimator(Animator animator)
+    {
+        this.animator = animator;
+    }
+
+    public void SetupSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+}
+
+[Serializable]
+public enum EnemyAnimationTrigger
+{
+    Attack,
+    Die
+}
